3e6779204da28559da30c128c30c7eceb2a3e533
J0SHUA-18 <deguzmanjoshua891@gmail.com>
2024-10-03 08:32:42 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using name space std;
+int main(){
+cout << "OUR LADY OF LOURDES COLLEGE FOUNDATION";
+cout << "COLLEGE OF COMPUTER STUDIES;
+cout << "BSIT 1 B";
+cout << "JOSHUA M. DE GUZMAN;
+cout << :P-3 SAN ISIDRO TALISAY CAMRINES NORTE;
+return 0;
+}
---
+++ 03VariableDeclaration.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	double age;
+	
+	cout << "Enter your age: ";
+	cin >> age;
+	cout << "Thank you!";
+return 0;
+}
